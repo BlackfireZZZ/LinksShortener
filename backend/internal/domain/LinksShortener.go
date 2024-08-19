e6c64d0e5dad9dc7b9f313f365a42ea508f631ac
Blackfire <Stipundrik@gmail.com>
2024-08-19 12:22:15 +0300

    repositories (inserting and querring from db)

---
+++ backend/internal/domain/LinksShortener.go
@@ -0,0 +1,6 @@
+package domain
+
+type LinksOut struct {
+	FullLink  string `json:"full_link"`
+	ShortLink string `json:"short_link"`
+}
--- backend/internal/repositories/LinksShortener.go
+++ backend/internal/repositories/LinksShortener.go
@@ -1,6 +1,11 @@
 package repositories
 
-import "github.com/jackc/pgx/v4/pgxpool"
+import (
+	"LinksShortener/internal/domain"
+	_ "LinksShortener/internal/domain"
+	"context"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
 
 type ShortenerRepository struct {
 	db *pgxpool.Pool
@@ -13,9 +18,30 @@ func NewShortenerRepository(db *pgxpool.Pool) *ShortenerRepository {
 }
 
 func (r *ShortenerRepository) SetLink(fullLink, shortLink string) (string, error) {
-	return "", nil
+	_, err := r.db.Exec(context.Background(), `INSERT INTO links (full_link, short_link) VALUES ($1, $2)`, fullLink, shortLink)
+	if err != nil {
+		return "", err
+	}
+	return shortLink, nil
 }
 
 func (r *ShortenerRepository) GetLinkIfExist(fullLink string) (shortLink string, isFound bool, err error) {
-	return "", true, nil
+	linksOut := &domain.LinksOut{}
+	rows, err := r.db.Query(context.Background(), `SELECT short_link FROM links WHERE full_link = $1`, fullLink)
+	if err != nil {
+		return "", false, err
+	}
+	defer rows.Close()
+
+	if !rows.Next() {
+		return "", false, nil
+	}
+	for rows.Next() {
+		err = rows.Scan(&linksOut)
+		if err != nil {
+			return "", false, err
+		}
+	}
+	shortLink = linksOut.ShortLink
+	return shortLink, true, nil
 }
