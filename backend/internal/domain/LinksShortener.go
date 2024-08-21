03326232b740c030d774ee3dff87e2fd688915c9
BlackfireZZZ <Stipundrik@gmail.com>
2024-08-21 13:55:41 +0300

    Deleted extra structure

--- backend/internal/domain/LinksShortener.go
+++ backend/internal/domain/LinksShortener.go
@@ -1,10 +1,5 @@
 package domain
 
-type LinksOut struct {
-	FullLink  string `json:"full_link"`
-	ShortLink string `json:"short_link"`
-}
-
 type LinksIn struct {
 	FullLink string `json:"full_link"`
 }
